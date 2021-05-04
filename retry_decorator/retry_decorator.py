bcacb463df6943a3d95997be31ee37c5b60da88e
Zachary Kneupper <zacharykneupper@Zacharys-MBP.lan>
2021-05-04 18:44:46 -0400

    Start decorator_retry

--- retry_decorator/retry_decorator.py
+++ retry_decorator/retry_decorator.py
@@ -7,4 +7,15 @@ import functools
 
 
 def retry(_func=None, max_tries=5, sleep=0):
-    pass
+    def decorator_retry(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+
+            pass
+
+        return wrapper
+
+    if _func is None:
+        return decorator_retry
+    else:
+        return decorator_retry(_func)
